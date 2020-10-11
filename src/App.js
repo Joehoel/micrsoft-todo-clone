503f85414641581ee60bd59ea9bca8fa3fe428a9
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-10-11 13:54:02 +0200

    Todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import TodoList from "./components/TodoList";
+import Sidebar from "./components/Sidebar";
+import "./App.css";
+import { useStateValue } from "./contexts";
+import { BrowserRouter as Router } from "react-router-dom";
+import Login from "./components/Login";
+import { db } from "./firebase";
+import { auth } from "./firebase";
+import { SET_ACTIVE_LIST, SET_USER } from "./reducers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [{ user, activeList }, dispatch] = useStateValue();
+
+	useEffect(() => {
+		db.collection("lists").onSnapshot(snapshot => {
+			const lists = snapshot.docs.map(doc => {
+				return {
+					id: doc.id,
+					name: doc.data().name,
+				};
+			});
+			dispatch({ type: SET_ACTIVE_LIST, activeList: lists[0] });
+		});
+
+		auth.onAuthStateChanged(authUser => {
+			if (authUser) {
+				dispatch({ type: SET_USER, user: authUser });
+			} else {
+				dispatch({ type: SET_USER, user: null });
+			}
+		});
+	}, []);
+
+	return (
+		<Router>
+			<div className="app__container">
+				{user && activeList ? (
+					<>
+						<Sidebar />
+						<TodoList id={activeList.id} name={activeList.name} />
+					</>
+				) : (
+					<Login />
+				)}
+			</div>
+		</Router>
+	);
 }
 
 export default App;
